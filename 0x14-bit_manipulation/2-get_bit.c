029fd82346ed09ac1519e953316845f4b0cf7a5b
ngarikev <kevin.gary191@gmail.com>
2023-04-08 09:05:33 -0700

    latest updated 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,11 +2,14 @@
 /**
  * get_bit - return the value of a bit at a given index
  * @n: number to find the bit
+ * @index: index of the bit
  * Return: value of index or -1 if error occured.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	int bit_vok
 	if (index > 63)
 		return (-1);
-	return (n >> index & 1);
+	bit_vok = (n >> index & 1);
+	return (bit_vok);
 }
