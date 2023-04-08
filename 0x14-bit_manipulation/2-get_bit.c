a38306ab4cacf4768008ec6d0990d6024cdaf5e8
ngarikev <kevin.gary191@gmail.com>
2023-04-08 09:09:17 -0700

    Lately updated 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,9 +7,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int bit_vok
 	if (index > 63)
 		return (-1);
-	bit_vok = (n >> index & 1);
-	return (bit_vok);
+	return (n >> index & 1);
 }
